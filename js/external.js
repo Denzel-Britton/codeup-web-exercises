7261beffe270c5095506006940911af7a918ab0f
Denzel-Britton <denzel.britton573@gmail.com>
2022-06-01 17:27:38 -0500

    complete: finished JS w/ HTML lecture

---
+++ js/external.js
@@ -0,0 +1,19 @@
+"use strict";
+console.log('Hello from JavaScript!')
+alert("Welcome to my Website!.");
+var userInput = prompt('What is your favorite color?:');
+console.log('The user entered: ' + userInput);
+alert("Great, "  + userInput + " that is mine too!.")
+var userInput = prompt('How many movies would you like to rent today?:');
+console.log('The user entered: ' + userInput);
+alert("Price of movies per day is $3.00 ")
+var userInput = prompt('How many hours did you work this week?:');
+console.log('The user entered: ' + userInput);
+alert("Total hours per week reflect how much money you earned over pay period.")
+var userInput = prompt('Does your class schedule conflict with this class?:');
+console.log('The user entered: ' + userInput);
+alert("You will not be enrolled if class is already full, or if this class conflicts with your schedule.If none of those apply you are now enrolled!")
+var userInput = prompt('How many items are you about to purchase?:');
+console.log('The user entered: ' + userInput);
+alert("A producet offer can be applied with 2 or more items.")
+alert("Premium members don't need a specific amount")
